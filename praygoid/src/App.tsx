471cf4ba5f1e75eba08ef5365968fe558e1428fe
fdhliakbar <fadhliakbar125@gmail.com>
2025-07-31 22:53:58 +0700

    Create project

---
+++ praygoid/src/App.tsx
@@ -0,0 +1,17 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to PrayGoid</h1>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <p>Your journey to spiritual growth starts here.</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
