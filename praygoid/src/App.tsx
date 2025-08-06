0b5c2ed4cd4a95a269d8bed75f17bde28660a050
fdhliakbar <fadhliakbar125@gmail.com>
2025-08-07 00:56:38 +0700

    remove animate from lofi and component for search bar

--- praygoid/src/App.tsx
+++ praygoid/src/App.tsx
@@ -1,20 +1,14 @@
 import './index.css'
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 
 function App() {
   return (
     <>
-      <h1>Where To Go</h1>
-      <img src="/src/assets/images/java-map.svg" alt="Java Map" />
-      <DotLottieReact>
-          src="https://app.lottiefiles.com/share/2489c221-1c10-4806-a2aa-ec92a0c092ce"
-          loop
-          autoplay
-      </DotLottieReact>
+    <div>
+      <h1>Hello World</h1>
+    </div>
     </>
   )
 }
-
 
 export default App
